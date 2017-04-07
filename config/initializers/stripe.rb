fb05615a7d20e4845739f512631f2a052296354e
khinezarthwe <koalakhine@gmail.com>
2017-04-07 13:45:33 +0900

    add env variable

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,7 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.secrets.publishable_key,
-  :secret_key      => Rails.application.secrets.secret_key 
+  #:publishable_key => Rails.application.secrets.publishable_key,
+  #:secret_key      => Rails.application.secrets.secret_key 
+  :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+  :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
